80c0c5031436c2a50134258a2850d423647ddc79
Dome <domenico.santarsiero@mail.polimi.it>
2024-06-04 22:10:48 +0200

    out directory

--- job.sh
+++ job.sh
@@ -3,7 +3,7 @@
 
 home_path="/g100/home/usertrain/a08trb58/"
 
-mkdir -p out
+mkdir -p $home_path/out
 
-singularity test $home_path"to_send/container.sif" 2> out/test.stderr > out/test.stdout
-singularity run $home_path"to_send/container.sif" 2> out/program.stderr > out/program.stdout
+singularity test $home_path"to_send/container.sif" 2> ../out/test.stderr > ../out/test.stdout
+singularity run $home_path"to_send/container.sif" 2> ../out/program.stderr > ../out/program.stdout
