269c57b05abd34f98cc5e7273591782eb523754a
Dome <domenico.santarsiero@mail.polimi.it>
2024-06-04 22:35:35 +0200

    SLURM

--- job.sh
+++ job.sh
@@ -1,5 +1,11 @@
-#!/bin/bash
-
+#!/bin/bash -l
+#SBATCH --cpus-per-task=1                                           # Set the number of CORES per task
+#SBATCH --nodes=1                                                   # number of nodes
+#SBATCH --ntasks=2                                                  # number of tasks
+#SBATCH --ntasks-per-node=2                                         # number of tasks per node
+#SBATCH --time=01:00:00                                             # time (HH:MM:SS)
+#SBATCH --partition=g100_usr_prod                                   # partition
+#SBATCH --account=trb58_sepolimi                                    # project account
 
 home_path="/g100/home/usertrain/a08trb58/"
 
