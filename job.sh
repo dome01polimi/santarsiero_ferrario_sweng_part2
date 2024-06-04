65c00c41419a5bda488dd04f38ced5954bdc2891
Ferraah <daniele6.ferrario@mail.polimi.it>
2024-06-04 18:27:40 +0000

    update

--- job.sh
+++ job.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-singularity  ../container.sif 2> test.stderr > test.stdout
-singularity  ../container.sif  2> program.stderr > program.stdout
+singularity run 'santarsiero_ferrario_sweng_part2/container.sif' 2> test.stderr > test.stdout
+singularity run 'santarsiero_ferrario_sweng_part2/container.sif' 2> program.stderr > program.stdout
