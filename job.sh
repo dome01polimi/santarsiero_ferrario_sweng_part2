203529474cbd644de309182833e80861eda88579
dome01polimi <145030697+dome01polimi@users.noreply.github.com>
2024-06-04 20:24:26 +0200

    Update job.sh

--- job.sh
+++ job.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-singularity santarsiero_ferrario_sweng_part2/container.sif 2> test.stderr > test.stdout
-singularity santarsiero_ferrario_sweng_part2/container.sif  2> program.stderr > program.stdout
+singularity  ../container.sif 2> test.stderr > test.stdout
+singularity  ../container.sif  2> program.stderr > program.stdout
