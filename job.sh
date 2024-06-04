70f005d785bb60a4316aa6518c7e3916725f7bb8
Dome <domenico.santarsiero@mail.polimi.it>
2024-06-04 21:14:55 +0200

    sbatch

--- job.sh
+++ job.sh
@@ -2,5 +2,5 @@
 
 home_path="/g100/home/usertrain/a08trb58/"
 
-singularity run $home_path"santarsiero_ferrario_sweng_part2/container.sif" 2> test.stderr > test.stdout
-singularity run $home_path"santarsiero_ferrario_sweng_part2/container.sif" 2> program.stderr > program.stdout
+singularity test $home_path"to_send/container.sif" 2> out/test.stderr > out/test.stdout
+singularity run $home_path"to_send/container.sif" 2> out/program.stderr > out/program.stdout
