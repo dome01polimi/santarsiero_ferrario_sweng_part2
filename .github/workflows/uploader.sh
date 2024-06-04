3a717091f78e4df2b804114e7858f6ab0b27cbd7
Ferraah <daniele6.ferrario@mail.polimi.it>
2024-06-04 15:05:44 +0000

    update

--- .github/workflows/uploader.sh
+++ .github/workflows/uploader.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/expect -f
 set filename [lindex $argv 0]
+set pass [lindex $argv 1]
+
 set timeout -1
 spawn scp -o StrictHostKeyChecking=no $filename a08trb58@login.g100.cineca.it:~/
-set pass $SSHGALILEO
 expect {
         password: {send "$pass\r" ; exp_continue}
 	        eof exit
