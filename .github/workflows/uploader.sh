dbdf21cb93c3d051e4aecff9d13e099a7ccd1fa8
Ferraah <daniele6.ferrario@mail.polimi.it>
2024-06-04 17:31:39 +0000

    update

--- .github/workflows/uploader.sh
+++ .github/workflows/uploader.sh
@@ -3,22 +3,20 @@
 # Get the password from the second argument
 set pass [lindex $argv 1]
 
-# Remove the password from the argument list
-set argv [lrange $argv 2 end]
+set filename [lindex $argv 2]
+
 
 # Set timeout to unlimited
 set timeout -1
 
 # Loop through each file in the modified argument list
-foreach filename $argv {
-    spawn scp -o StrictHostKeyChecking=no -r $filename a08trb58@login.g100.cineca.it:~/
-    expect {
-        "password:" {
-            send "$pass\r"
-            exp_continue
-        }
-        eof {
-            exit
-        }
+spawn scp -o StrictHostKeyChecking=no -r $filename a08trb58@login.g100.cineca.it:~/
+expect {
+    "password:" {
+        send "$pass\r"
+        exp_continue
+    }
+    eof {
+        exit
     }
 }
