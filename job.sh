b59e0c2d5e62f24a06e5c5ca1586ade0458965aa
Dome <domenico.santarsiero@mail.polimi.it>
2024-06-04 21:50:59 +0200

    out directory

--- job.sh
+++ job.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+
 home_path="/g100/home/usertrain/a08trb58/"
+
+mkdir -p out
 
 singularity test $home_path"to_send/container.sif" 2> out/test.stderr > out/test.stdout
 singularity run $home_path"to_send/container.sif" 2> out/program.stderr > out/program.stdout
