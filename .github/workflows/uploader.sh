3293c855cc734dd0e41c497e9d273e6161482598
Ferraah <daniele6.ferrario@mail.polimi.it>
2024-06-04 15:16:46 +0000

    update

--- .github/workflows/uploader.sh
+++ .github/workflows/uploader.sh
@@ -1,10 +1,24 @@
 #!/usr/bin/expect -f
-set filename [lindex $argv 0]
+
+# Get the password from the second argument
 set pass [lindex $argv 1]
 
+# Remove the password from the argument list
+set argv [lrange $argv 2 end]
+
+# Set timeout to unlimited
 set timeout -1
-spawn scp -o StrictHostKeyChecking=no $filename a08trb58@login.g100.cineca.it:~/
-expect {
-        password: {send "$pass\r" ; exp_continue}
-	        eof exit
-	}
+
+# Loop through each file in the modified argument list
+foreach filename $argv {
+    spawn scp -o StrictHostKeyChecking=no $filename a08trb58@login.g100.cineca.it:~/
+    expect {
+        "password:" {
+            send "$pass\r"
+            exp_continue
+        }
+        eof {
+            exit
+        }
+    }
+}
