b65f446bf3c23cddb77e5e784fc689705da36562
Dome <domenico.santarsiero@mail.polimi.it>
2024-06-05 17:07:20 +0200

    log on Github Action

--- job.sh
+++ job.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/bash -l
 #SBATCH --job-name=run-Singularity-container                        # Job name
 #SBATCH --cpus-per-task=1                                           # Set the number of CORES per task
 #SBATCH --nodes=1                                                   # number of nodes
 #SBATCH --ntasks=2                                                  # number of tasks
 #SBATCH --ntasks-per-node=2                                         # number of tasks per node
-#SBATCH --time=01:00:00                                             # Set a limit on the total run time
+#SBATCH --time=00:10:00                                             # Set a limit on the total run time
 #SBATCH --partition=g100_all_serial                                 # partition
 #SBATCH --account=tra24_sepolimi                                    # project account
 #SBATCH -o out/%x-%j.out                                            # File to which STDOUT will be written
