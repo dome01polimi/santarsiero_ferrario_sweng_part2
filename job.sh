857920cf1e5030e83564f9a78829f47106609fb4
Dome <domenico.santarsiero@mail.polimi.it>
2024-06-05 14:03:34 +0200

    SLURM

--- job.sh
+++ job.sh
@@ -8,7 +8,7 @@
 #SBATCH --partition=g100_all_serial                                 # partition
 #SBATCH --account=tra24_sepolimi                                    # project account
 #SBATCH -o out/%x-%j.out                                         # File to which STDOUT will be written
-#SBATCH -e out/%x-%j.err                                         # File to which STDERROR will be written
+#SBATCH -e out/%x-%j.err                                         # File to which STDERR will be written
 
 export HOME_PATH ="/g100/home/usertrain/a08trb58/"
 
