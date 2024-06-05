c0c9866eded59a959132d9907feb1b46fc740354
Dome <domenico.santarsiero@mail.polimi.it>
2024-06-05 13:12:15 +0200

    SLURM

--- job.sh
+++ job.sh
@@ -1,13 +1,14 @@
-#!/bin/bash -l
+/bin/bash
+#SBATCH --job-name=run-Singularity-container                        # Job name
 #SBATCH --cpus-per-task=1                                           # Set the number of CORES per task
 #SBATCH --nodes=1                                                   # number of nodes
 #SBATCH --ntasks=2                                                  # number of tasks
 #SBATCH --ntasks-per-node=2                                         # number of tasks per node
-#SBATCH --time=01:00:00                                             # time (HH:MM:SS)
+#SBATCH --time=01:00:00                                             # Set a limit on the total run time
 #SBATCH --partition=g100_all_serial                                 # partition
 #SBATCH --account=tra24_sepolimi                                    # project account
-#SBATCH -o ../out/%x-%j.out # File to which STDOUT will be written
-#SBATCH -e ../out/%x-%j.err # File to which STDERR will be written
+#SBATCH -o ../out/%x-%j.out                                         # File to which STDOUT will be written
+#SBATCH -e ../out/%x-%j.err                                         # File to which STDERROR will be written
 
 home_path="/g100/home/usertrain/a08trb58/"
 
