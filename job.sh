bf3cdb22993fbecda2ea266642b929ab23e28b38
Dome <domenico.santarsiero@mail.polimi.it>
2024-06-05 12:46:45 +0200

    SLURM

--- job.sh
+++ job.sh
@@ -11,5 +11,5 @@ home_path="/g100/home/usertrain/a08trb58/"
 
 mkdir -p $home_path/out
 
-singularity test $home_path"to_send/container.sif" 2> ../out/test.stderr > ../out/test.stdout 
-singularity run $home_path"to_send/container.sif" 2> ../out/program.stderr > ../out/program.stdout
+singularity test -C $home_path"to_send/container.sif" 2> ../out/test.stderr > ../out/test.stdout 
+singularity run -C $home_path"to_send/container.sif" 2> ../out/program.stderr > ../out/program.stdout
